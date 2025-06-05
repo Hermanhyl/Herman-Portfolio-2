d1a968062284b3f5e1bbea5935875974f5188fa4
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-06-05 17:44:43 +0200

    Feat: padding fix, project display cards

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,7 @@ import { projects } from "../../data/projects/projects";
 
 function Home() {
   return (
-    <div className="px-6 pt-40 pb-10 flex flex-col items-center">
+    <div className="px-6 pt-20 pb-10 flex flex-col items-center">
       <h1 className="text-4xl font-bold mb-10 text-center">Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-10 w-full px-16">
         {projects.map((project) => (

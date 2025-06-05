6703156415cadd0a369055a91331eb450cbb3a47
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-06-05 19:28:21 +0200

    style: changed padding for projects grid

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="px-6 pt-20 pb-10 flex flex-col items-center">
       <h1 className="text-4xl font-bold mb-10 text-center">Projects</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-10 w-full px-16">
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-10 w-full ">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}

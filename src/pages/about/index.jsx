80045424fad3ab6c91ac2fa070c51507253b3dc4
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-06-05 19:27:38 +0200

    Feat: changed text direction

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -5,16 +5,16 @@ function About() {
         <h1 className="text-4xl md:text-5xl font-extrabold mb-6 bg-gradient-to-r from-emerald-400 via-cyan-400 to-purple-500 text-transparent bg-clip-text">
           About Me
         </h1>
-        <p className="text-lg md:text-xl text-gray-100 mb-4">
+        <p className="text-lg text-left md:text-xl text-gray-100 mb-4">
           Hi, I'm a 27-year-old aspiring front-end developer and digital designer. This portfolio was created to showcase the projects I’ve worked on during my two years of study.
         </p>
-        <p className="text-lg md:text-xl text-gray-100 mb-4">
+        <p className="text-lg text-left md:text-xl text-gray-100 mb-4">
           The projects reflect not only my technical growth, but also how I’ve evolved creatively—learning to turn ideas into accessible, interactive, and visually engaging web experiences.
         </p>
-        <p className="text-lg md:text-xl text-gray-100 mb-4">
+        <p className="text-lg text-left md:text-xl text-gray-100 mb-4">
           You'll find a selection of applications, UI concepts, and illustrations that demonstrate my abilities with React, Tailwind CSS, design systems, and user-centered thinking.
         </p>
-        <p className="text-lg md:text-xl text-gray-100">
+        <p className="text-lg text-left md:text-xl text-gray-100">
           I'm currently looking for opportunities in front-end development or digital design roles where I can continue to grow, collaborate, and contribute to meaningful projects.
         </p>
       </div>

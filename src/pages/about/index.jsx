7b2069a5a67fb34f54508b97e1147c993c74d2ea
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2025-07-05 11:29:15 +0200

    updated info in data file

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,12 +1,12 @@
 function About() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-b from-black via-gray-900 to-black px-4 py-20 text-white">
-      <div className="w-full max-w-3xl backdrop-blur-md bg-white/10 p-8 rounded-2xl border border-white/20 shadow-lg text-center">
+      <div className="animated-border w-full max-w-3xl p-8 rounded-2xl text-center text-white relative z-10 bg-white/10 backdrop-blur-md">
         <h1 className="text-4xl md:text-5xl font-extrabold mb-6 bg-gradient-to-r from-emerald-400 via-cyan-400 to-purple-500 text-transparent bg-clip-text">
           About Me
         </h1>
         <p className="text-lg text-left md:text-xl text-gray-100 mb-4">
-          Hi, I'm a 27-year-old aspiring front-end developer and digital designer. This portfolio was created to showcase the projects I’ve worked on during my two years of study.
+          Hi, I'm a 27-year-old aspiring front-end developer and digital designer. This portfolio was created to showcase some of the projects I’ve worked on during my two years of study.
         </p>
         <p className="text-lg text-left md:text-xl text-gray-100 mb-4">
           The projects reflect not only my technical growth, but also how I’ve evolved creatively—learning to turn ideas into accessible, interactive, and visually engaging web experiences.
@@ -19,6 +19,10 @@ function About() {
         </p>
       </div>
     </div>
+
+
+
+
   );
 }
 
